5d05b73325d1bad6d5cc4e128d1e1dd1f75127db
cathaysia <318513897@outlook.com>
2021-01-07 20:45:07 +0800

    bookmarks support

--- preload.js
+++ preload.js
@@ -1,13 +1,22 @@
+/**
+ * 参考：
+ * https://cloud.tencent.com/developer/ask/187017
+ * https://support.mozilla.org/en-US/kb/bookmarks-firefox#w_how-do-i-find-my-bookmarks
+ */
+
 const sqlite3 = require('sqlite3').verbose()
 const path = require('path')
-let fs = require('fs')
+const fs = require('fs')
 
-let dataCache = null
+const historyQuerySql = "SELECT title, url, description FROM moz_places a JOIN moz_historyvisits b on a.id = b.place_id WHERE a.title is not null ORDER BY visit_date DESC;"
+const bookMarksQuerySql = "SELECT a.title, b.url, b.description FROM moz_bookmarks a JOIN moz_places b ON a.fk = b.id ORDER BY b.last_visit_date DESC;"
 
-let getDataPath = new Promise(function (resolve) {
-    // 这里如果不实用 Promise 会导致返回值为 undefined
+let resultList = []
+
+let getDataPath = new Promise(resolve => {
+    // 这里如果不使用 Promise 会导致返回值为 undefined
     mozPath = path.join(process.env.APPDATA, 'Mozilla/Firefox/Profiles')
-    fs.readdirSync(mozPath).forEach(function (ele, index) {
+    fs.readdirSync(mozPath).forEach((ele, index) => {
         if (fs.statSync(path.join(mozPath, ele)).isDirectory) {
             let dbPath = path.join(mozPath, ele, 'places.sqlite')
             if (fs.existsSync(dbPath)) {
@@ -17,26 +26,43 @@ let getDataPath = new Promise(function (resolve) {
     })
 })
 
-function getFirefoxData(firefoxDataPath) {
-    // 一般来说 ff 历史记录保持在 "%appdata%/Mozilla/Firefox/Profiles/.*/places.sqlite中"
-    let ffData = [];
+const percent2percent25 = (URI) => {
+    try {
+        let str = decodeURI(URI)
+        return str
+    } catch (err) {
+        return URI
+    }
+}
+
+function getFirefoxData(firefoxDataPath, querySql) {
     let db_sqlite3 = new sqlite3.Database(firefoxDataPath)
-    db_sqlite3.all("SELECT title, url, description, visit_date FROM moz_places a JOIN moz_historyvisits b on a.id = b.id WHERE a.title is not null ORDER BY visit_date DESC;", function (err, rows) {
-        // 这里按时间进行排序，时间近的排在前面
-        if (err) throw err;
-        rows.forEach(c => {
-            ffData.push({
-                title: c.title,
-                description: c.description || c.url, // 如果 description 为 null ，就用 url
+    db_sqlite3.all(querySql, (err, rows) => {
+        if (err) throw err
+        rows.forEach(item => {
+            resultList.push({
+                title: item.title,
+                // description: item.description || item.url, // 如果 description 为 null ，就用 url
+                description: percent2percent25(item.url),
                 icon: "./icon.png",
-                url: c.url,
-                lowcaseTitle: c.title.toLowerCase(),
-                visitDate: c.visit_date // 访问的时间
+                url: item.url,
+                lowcaseTitle: item.title.toLowerCase(),
             })
         })
     })
     db_sqlite3.close()
-    dataCache = ffData
+}
+
+let searchAction = (action, searchWord, callbackSetList) => {
+    searchWord = searchWord.trim()
+    return callbackSetList(resultList.filter(item => item.lowcaseTitle.includes(searchWord) | item.url.includes(searchWord)))
+    // 在进行过滤时，使用 title 和 url 进行过滤
+}
+let selectAction = (action, itemData, callbackSetList) => {
+    window.utools.hideMainWindow()
+    const url = itemData.url
+    require('electron').shell.openExternal(url)
+    window.utools.outPlugin()
 }
 
 window.exports = {
@@ -44,22 +70,36 @@ window.exports = {
         mode: "list",
         args: {
             enter: (action, callbackSetList) => {
-                getDataPath.then(function (el) {
-                    getFirefoxData(el)
-                    return callbackSetList(dataCache)
+                resultList = []
+                getDataPath.then(path => getFirefoxData(path, historyQuerySql))
+            },
+            search: searchAction,
+            select: selectAction
+        }
+    },
+    "ff_bookmarks": {
+        mode: "list",
+        args: {
+            enter: (action, callbackSetList) => {
+                resultList = []
+                getDataPath.then(path => getFirefoxData(path, bookMarksQuerySql))
+            },
+            search: searchAction,
+            select: selectAction
+        }
+    },
+    "ff_all": {
+        mode: "list",
+        args: {
+            enter: (action, callbackSetList) => {
+                resultList = []
+                getDataPath.then(path => {
+                    getFirefoxData(path, historyQuerySql)
+                    getFirefoxData(path, bookMarksQuerySql)
                 })
             },
-            search: (action, searchWord, callbackSetList) => {
-                searchWord = searchWord.trim()
-                return callbackSetList(dataCache.filter(x => x.lowcaseTitle.includes(searchWord) | x.url.includes(searchWord)))
-                // 在进行过滤时，使用 title 和 url 进行过滤
-            },
-            select: (action, itemData, callbackSetList) => {
-                window.utools.hideMainWindow()
-                const url = itemData.url
-                require('electron').shell.openExternal(url)
-                window.utools.outPlugin()
-            }
+            search: searchAction,
+            select: selectAction
         }
     }
 }
